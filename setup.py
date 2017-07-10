f93f8b0c0bfdd4bdb643c7baeac399bd4a051f9a
Hendrik Wiese <hendrik.wiese@dfki.de>
2017-07-10 14:19:43 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name = "docker-preproc",
+      version = "1 0 0",
+      description = "preprocessor for dockerfiles",
+      author = "Hendrik Wiese <hendrik.wiese@dfki.de>",
+      url = "",
+      scripts = ["dpp.py"],
+      )
+
